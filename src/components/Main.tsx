95845aac0cdf12848c05940cbe4be244a6876623
Vinicius Gabriel <viiniciusgs03@gmail.com>
2022-03-01 16:19:49 -0300

    :recycle: add handle classname function in main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -143,6 +143,18 @@ export default function Main() {
     }
   }
 
+  const handleClassName = (row: number, index: number) => {
+    if (word && word[row] && word[row][index]) {
+      if (word[row][index].success) {
+        return 'success'
+      } else if (word[row][index].alert) {
+        return 'alert'
+      } else if (word[row][index].error) {
+        return 'error'
+      }
+    }
+  }
+
   useEffect(() => {
     window.addEventListener('keydown', handleKeyPress)
 
@@ -154,374 +166,104 @@ export default function Main() {
   return (
     <Container>
       <Row>
-        <Letter
-          className={
-            word &&
-            word[0] &&
-            word[0][0] &&
-            ((word[0][0].success && 'success') ||
-              (word[0][0].alert && 'alert') ||
-              (word[0][0].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(0, 0)}>
           {word && word[0] && word[0][0] && word[0][0].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[0] &&
-            word[0][1] &&
-            ((word[0][1].success && 'success') ||
-              (word[0][1].alert && 'alert') ||
-              (word[0][1].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(0, 1)}>
           {word && word[0] && word[0][1] && word[0][1].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[0] &&
-            word[0][2] &&
-            ((word[0][2].success && 'success') ||
-              (word[0][2].alert && 'alert') ||
-              (word[0][2].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(0, 2)}>
           {word && word[0] && word[0][2] && word[0][2].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[0] &&
-            word[0][3] &&
-            ((word[0][3].success && 'success') ||
-              (word[0][3].alert && 'alert') ||
-              (word[0][3].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(0, 3)}>
           {word && word[0] && word[0][3] && word[0][3].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[0] &&
-            word[0][4] &&
-            ((word[0][4].success && 'success') ||
-              (word[0][4].alert && 'alert') ||
-              (word[0][4].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(0, 4)}>
           {word && word[0] && word[0][4] && word[0][4].letter}
         </Letter>
       </Row>
       <Row>
-        <Letter
-          className={
-            word &&
-            word[1] &&
-            word[1][0] &&
-            ((word[1][0].success && 'success') ||
-              (word[1][0].alert && 'alert') ||
-              (word[1][0].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(1, 0)}>
           {word && word[1] && word[1][0] && word[1][0].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[1] &&
-            word[1][1] &&
-            ((word[1][1].success && 'success') ||
-              (word[1][1].alert && 'alert') ||
-              (word[1][1].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(1, 1)}>
           {word && word[1] && word[1][1] && word[1][1].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[1] &&
-            word[1][2] &&
-            ((word[1][2].success && 'success') ||
-              (word[1][2].alert && 'alert') ||
-              (word[1][2].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(1, 2)}>
           {word && word[1] && word[1][2] && word[1][2].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[1] &&
-            word[1][3] &&
-            ((word[1][3].success && 'success') ||
-              (word[1][3].alert && 'alert') ||
-              (word[1][3].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(1, 3)}>
           {word && word[1] && word[1][3] && word[1][3].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[1] &&
-            word[1][4] &&
-            ((word[1][4].success && 'success') ||
-              (word[1][4].alert && 'alert') ||
-              (word[1][4].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(1, 4)}>
           {word && word[1] && word[1][4] && word[1][4].letter}
         </Letter>
       </Row>
       <Row>
-        <Letter
-          className={
-            word &&
-            word[2] &&
-            word[2][0] &&
-            ((word[2][0].success && 'success') ||
-              (word[2][0].alert && 'alert') ||
-              (word[2][0].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(2, 0)}>
           {word && word[2] && word[2][0] && word[2][0].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[2] &&
-            word[2][1] &&
-            ((word[2][1].success && 'success') ||
-              (word[2][1].alert && 'alert') ||
-              (word[2][1].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(2, 1)}>
           {word && word[2] && word[2][1] && word[2][1].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[2] &&
-            word[2][2] &&
-            ((word[2][2].success && 'success') ||
-              (word[2][2].alert && 'alert') ||
-              (word[2][2].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(2, 2)}>
           {word && word[2] && word[2][2] && word[2][2].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[2] &&
-            word[2][3] &&
-            ((word[2][3].success && 'success') ||
-              (word[2][3].alert && 'alert') ||
-              (word[2][3].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(2, 3)}>
           {word && word[2] && word[2][3] && word[2][3].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[2] &&
-            word[2][4] &&
-            ((word[2][4].success && 'success') ||
-              (word[2][4].alert && 'alert') ||
-              (word[2][4].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(2, 4)}>
           {word && word[2] && word[2][4] && word[2][4].letter}
         </Letter>
       </Row>
       <Row>
-        <Letter
-          className={
-            word &&
-            word[3] &&
-            word[3][0] &&
-            ((word[3][0].success && 'success') ||
-              (word[3][0].alert && 'alert') ||
-              (word[3][0].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(3, 0)}>
           {word && word[3] && word[3][0] && word[3][0].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[3] &&
-            word[3][1] &&
-            ((word[3][1].success && 'success') ||
-              (word[3][1].alert && 'alert') ||
-              (word[3][1].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(3, 1)}>
           {word && word[3] && word[3][1] && word[3][1].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[3] &&
-            word[3][2] &&
-            ((word[3][2].success && 'success') ||
-              (word[3][2].alert && 'alert') ||
-              (word[3][2].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(3, 2)}>
           {word && word[3] && word[3][2] && word[3][2].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[3] &&
-            word[3][3] &&
-            ((word[3][3].success && 'success') ||
-              (word[3][3].alert && 'alert') ||
-              (word[3][3].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(3, 3)}>
           {word && word[3] && word[3][3] && word[3][3].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[3] &&
-            word[3][4] &&
-            ((word[3][4].success && 'success') ||
-              (word[3][4].alert && 'alert') ||
-              (word[3][4].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(3, 4)}>
           {word && word[3] && word[3][4] && word[3][4].letter}
         </Letter>
       </Row>
       <Row>
-        <Letter
-          className={
-            word &&
-            word[4] &&
-            word[4][0] &&
-            ((word[4][0].success && 'success') ||
-              (word[4][0].alert && 'alert') ||
-              (word[4][0].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(4, 0)}>
           {word && word[4] && word[4][0] && word[4][0].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[4] &&
-            word[4][1] &&
-            ((word[4][1].success && 'success') ||
-              (word[4][1].alert && 'alert') ||
-              (word[4][1].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(4, 1)}>
           {word && word[4] && word[4][1] && word[4][1].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[4] &&
-            word[4][2] &&
-            ((word[4][2].success && 'success') ||
-              (word[4][2].alert && 'alert') ||
-              (word[4][2].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(4, 2)}>
           {word && word[4] && word[4][2] && word[4][2].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[4] &&
-            word[4][3] &&
-            ((word[4][3].success && 'success') ||
-              (word[4][3].alert && 'alert') ||
-              (word[4][3].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(4, 3)}>
           {word && word[4] && word[4][3] && word[4][3].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[4] &&
-            word[4][4] &&
-            ((word[4][4].success && 'success') ||
-              (word[4][4].alert && 'alert') ||
-              (word[4][4].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(4, 4)}>
           {word && word[4] && word[4][4] && word[4][4].letter}
         </Letter>
       </Row>
       <Row>
-        <Letter
-          className={
-            word &&
-            word[5] &&
-            word[5][0] &&
-            ((word[5][0].success && 'success') ||
-              (word[5][0].alert && 'alert') ||
-              (word[5][0].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(5, 0)}>
           {word && word[5] && word[5][0] && word[5][0].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[5] &&
-            word[5][1] &&
-            ((word[5][1].success && 'success') ||
-              (word[5][1].alert && 'alert') ||
-              (word[5][1].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(5, 1)}>
           {word && word[5] && word[5][1] && word[5][1].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[5] &&
-            word[5][2] &&
-            ((word[5][2].success && 'success') ||
-              (word[5][2].alert && 'alert') ||
-              (word[5][2].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(5, 2)}>
           {word && word[5] && word[5][2] && word[5][2].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[5] &&
-            word[5][3] &&
-            ((word[5][3].success && 'success') ||
-              (word[5][3].alert && 'alert') ||
-              (word[5][3].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(5, 3)}>
           {word && word[5] && word[5][3] && word[5][3].letter}
         </Letter>
-        <Letter
-          className={
-            word &&
-            word[5] &&
-            word[5][4] &&
-            ((word[5][4].success && 'success') ||
-              (word[5][4].alert && 'alert') ||
-              (word[5][4].error && 'error'))
-          }
-        >
+        <Letter className={handleClassName(5, 4)}>
           {word && word[5] && word[5][4] && word[5][4].letter}
         </Letter>
       </Row>
