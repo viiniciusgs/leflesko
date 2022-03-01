e876885e8e871d85ab51a17869ed2434562e97cd
Vinicius Gabriel <viiniciusgs03@gmail.com>
2022-03-01 15:15:43 -0300

    :sparkles: add handle submit function in main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,12 +9,98 @@ interface Word {
   error: boolean
 }
 
+const mock = 'teste'
+
 export default function Main() {
   const [word, setWord] = useState<Word[][]>()
   const [index, setIndex] = useState<number>(0)
+  const [finished, setFinished] = useState<boolean>(false)
+
+  const correctWordSplit = mock.toLowerCase().split('')
+
+  const correctWord = [
+    {
+      letter: correctWordSplit[0],
+      success: false,
+      alert: false,
+    },
+    {
+      letter: correctWordSplit[1],
+      success: false,
+      alert: false,
+    },
+    {
+      letter: correctWordSplit[2],
+      success: false,
+      alert: false,
+    },
+    {
+      letter: correctWordSplit[3],
+      success: false,
+      alert: false,
+    },
+    {
+      letter: correctWordSplit[4],
+      success: false,
+      alert: false,
+    },
+  ]
+
+  const handleSubmit = () => {
+    if (word && word[index].length === 5) {
+      for (let i = 0; i <= word[index].length - 1; i++) {
+        for (let j = 0; j <= word[index].length - 1; j++) {
+          if (word[index][i].letter === correctWord[j].letter && i === j) {
+            word[index][i].success = true
+            correctWord[j].success = true
+          } else {
+            word[index][i].error = true
+          }
+        }
+      }
+
+      for (let i = 0; i <= word[index].length - 1; i++) {
+        for (let j = 0; j <= word[index].length - 1; j++) {
+          if (
+            word[index][i].letter === correctWord[j].letter &&
+            !correctWord[j].success &&
+            !correctWord[j].alert &&
+            !word[index][i].success &&
+            !word[index][i].alert
+          ) {
+            word[index][i].alert = true
+            correctWord[j].alert = true
+          } else {
+            word[index][i].error = true
+          }
+        }
+      }
+
+      const wordInString =
+        word[index][0].letter +
+        word[index][1].letter +
+        word[index][2].letter +
+        word[index][3].letter +
+        word[index][4].letter
+
+      if (wordInString === mock) {
+        console.log('palavra correta')
+        setFinished(true)
+      } else if (index < 5) {
+        setIndex(index + 1)
+      } else {
+        console.log('palavra incorreta')
+        setFinished(true)
+      }
+    }
+  }
 
   const handleKeyPress = (event: KeyboardEvent) => {
     const { key, keyCode } = event
+
+    if (finished) {
+      return
+    }
 
     if (keyCode >= 65 && keyCode <= 90) {
       if (!word) {
@@ -50,10 +136,10 @@ export default function Main() {
 
         setWord([...word])
       }
-    }
-
-    if (keyCode === 8 && word && word[index] && word[index][0]) {
+    } else if (keyCode === 8 && word && word[index] && word[index][0]) {
       setWord(word.slice(0, index).concat([word[index].slice(0, -1)]))
+    } else if (keyCode === 13) {
+      handleSubmit()
     }
   }
 
@@ -68,56 +154,376 @@ export default function Main() {
   return (
     <Container>
       <Row>
-        <Letter className="alert">
+        <Letter
+          className={
+            word &&
+            word[0] &&
+            word[0][0] &&
+            ((word[0][0].success && 'success') ||
+              (word[0][0].alert && 'alert') ||
+              (word[0][0].error && 'error'))
+          }
+        >
           {word && word[0] && word[0][0] && word[0][0].letter}
         </Letter>
-        <Letter className="success">
+        <Letter
+          className={
+            word &&
+            word[0] &&
+            word[0][1] &&
+            ((word[0][1].success && 'success') ||
+              (word[0][1].alert && 'alert') ||
+              (word[0][1].error && 'error'))
+          }
+        >
           {word && word[0] && word[0][1] && word[0][1].letter}
         </Letter>
-        <Letter className="alert">
+        <Letter
+          className={
+            word &&
+            word[0] &&
+            word[0][2] &&
+            ((word[0][2].success && 'success') ||
+              (word[0][2].alert && 'alert') ||
+              (word[0][2].error && 'error'))
+          }
+        >
           {word && word[0] && word[0][2] && word[0][2].letter}
         </Letter>
-        <Letter className="alert">
+        <Letter
+          className={
+            word &&
+            word[0] &&
+            word[0][3] &&
+            ((word[0][3].success && 'success') ||
+              (word[0][3].alert && 'alert') ||
+              (word[0][3].error && 'error'))
+          }
+        >
           {word && word[0] && word[0][3] && word[0][3].letter}
         </Letter>
-        <Letter className="error">
+        <Letter
+          className={
+            word &&
+            word[0] &&
+            word[0][4] &&
+            ((word[0][4].success && 'success') ||
+              (word[0][4].alert && 'alert') ||
+              (word[0][4].error && 'error'))
+          }
+        >
           {word && word[0] && word[0][4] && word[0][4].letter}
         </Letter>
       </Row>
       <Row>
-        <Letter className="error">i</Letter>
-        <Letter className="error">p</Letter>
-        <Letter className="error">s</Letter>
-        <Letter className="error">u</Letter>
-        <Letter className="error">m</Letter>
+        <Letter
+          className={
+            word &&
+            word[1] &&
+            word[1][0] &&
+            ((word[1][0].success && 'success') ||
+              (word[1][0].alert && 'alert') ||
+              (word[1][0].error && 'error'))
+          }
+        >
+          {word && word[1] && word[1][0] && word[1][0].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[1] &&
+            word[1][1] &&
+            ((word[1][1].success && 'success') ||
+              (word[1][1].alert && 'alert') ||
+              (word[1][1].error && 'error'))
+          }
+        >
+          {word && word[1] && word[1][1] && word[1][1].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[1] &&
+            word[1][2] &&
+            ((word[1][2].success && 'success') ||
+              (word[1][2].alert && 'alert') ||
+              (word[1][2].error && 'error'))
+          }
+        >
+          {word && word[1] && word[1][2] && word[1][2].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[1] &&
+            word[1][3] &&
+            ((word[1][3].success && 'success') ||
+              (word[1][3].alert && 'alert') ||
+              (word[1][3].error && 'error'))
+          }
+        >
+          {word && word[1] && word[1][3] && word[1][3].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[1] &&
+            word[1][4] &&
+            ((word[1][4].success && 'success') ||
+              (word[1][4].alert && 'alert') ||
+              (word[1][4].error && 'error'))
+          }
+        >
+          {word && word[1] && word[1][4] && word[1][4].letter}
+        </Letter>
       </Row>
       <Row>
-        <Letter className="success">d</Letter>
-        <Letter className="success">o</Letter>
-        <Letter className="success">l</Letter>
-        <Letter className="success">o</Letter>
-        <Letter className="success">r</Letter>
+        <Letter
+          className={
+            word &&
+            word[2] &&
+            word[2][0] &&
+            ((word[2][0].success && 'success') ||
+              (word[2][0].alert && 'alert') ||
+              (word[2][0].error && 'error'))
+          }
+        >
+          {word && word[2] && word[2][0] && word[2][0].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[2] &&
+            word[2][1] &&
+            ((word[2][1].success && 'success') ||
+              (word[2][1].alert && 'alert') ||
+              (word[2][1].error && 'error'))
+          }
+        >
+          {word && word[2] && word[2][1] && word[2][1].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[2] &&
+            word[2][2] &&
+            ((word[2][2].success && 'success') ||
+              (word[2][2].alert && 'alert') ||
+              (word[2][2].error && 'error'))
+          }
+        >
+          {word && word[2] && word[2][2] && word[2][2].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[2] &&
+            word[2][3] &&
+            ((word[2][3].success && 'success') ||
+              (word[2][3].alert && 'alert') ||
+              (word[2][3].error && 'error'))
+          }
+        >
+          {word && word[2] && word[2][3] && word[2][3].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[2] &&
+            word[2][4] &&
+            ((word[2][4].success && 'success') ||
+              (word[2][4].alert && 'alert') ||
+              (word[2][4].error && 'error'))
+          }
+        >
+          {word && word[2] && word[2][4] && word[2][4].letter}
+        </Letter>
       </Row>
       <Row>
-        <Letter></Letter>
-        <Letter></Letter>
-        <Letter></Letter>
-        <Letter></Letter>
-        <Letter></Letter>
+        <Letter
+          className={
+            word &&
+            word[3] &&
+            word[3][0] &&
+            ((word[3][0].success && 'success') ||
+              (word[3][0].alert && 'alert') ||
+              (word[3][0].error && 'error'))
+          }
+        >
+          {word && word[3] && word[3][0] && word[3][0].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[3] &&
+            word[3][1] &&
+            ((word[3][1].success && 'success') ||
+              (word[3][1].alert && 'alert') ||
+              (word[3][1].error && 'error'))
+          }
+        >
+          {word && word[3] && word[3][1] && word[3][1].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[3] &&
+            word[3][2] &&
+            ((word[3][2].success && 'success') ||
+              (word[3][2].alert && 'alert') ||
+              (word[3][2].error && 'error'))
+          }
+        >
+          {word && word[3] && word[3][2] && word[3][2].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[3] &&
+            word[3][3] &&
+            ((word[3][3].success && 'success') ||
+              (word[3][3].alert && 'alert') ||
+              (word[3][3].error && 'error'))
+          }
+        >
+          {word && word[3] && word[3][3] && word[3][3].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[3] &&
+            word[3][4] &&
+            ((word[3][4].success && 'success') ||
+              (word[3][4].alert && 'alert') ||
+              (word[3][4].error && 'error'))
+          }
+        >
+          {word && word[3] && word[3][4] && word[3][4].letter}
+        </Letter>
       </Row>
       <Row>
-        <Letter></Letter>
-        <Letter></Letter>
-        <Letter></Letter>
-        <Letter></Letter>
-        <Letter></Letter>
+        <Letter
+          className={
+            word &&
+            word[4] &&
+            word[4][0] &&
+            ((word[4][0].success && 'success') ||
+              (word[4][0].alert && 'alert') ||
+              (word[4][0].error && 'error'))
+          }
+        >
+          {word && word[4] && word[4][0] && word[4][0].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[4] &&
+            word[4][1] &&
+            ((word[4][1].success && 'success') ||
+              (word[4][1].alert && 'alert') ||
+              (word[4][1].error && 'error'))
+          }
+        >
+          {word && word[4] && word[4][1] && word[4][1].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[4] &&
+            word[4][2] &&
+            ((word[4][2].success && 'success') ||
+              (word[4][2].alert && 'alert') ||
+              (word[4][2].error && 'error'))
+          }
+        >
+          {word && word[4] && word[4][2] && word[4][2].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[4] &&
+            word[4][3] &&
+            ((word[4][3].success && 'success') ||
+              (word[4][3].alert && 'alert') ||
+              (word[4][3].error && 'error'))
+          }
+        >
+          {word && word[4] && word[4][3] && word[4][3].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[4] &&
+            word[4][4] &&
+            ((word[4][4].success && 'success') ||
+              (word[4][4].alert && 'alert') ||
+              (word[4][4].error && 'error'))
+          }
+        >
+          {word && word[4] && word[4][4] && word[4][4].letter}
+        </Letter>
       </Row>
       <Row>
-        <Letter></Letter>
-        <Letter></Letter>
-        <Letter></Letter>
-        <Letter></Letter>
-        <Letter></Letter>
+        <Letter
+          className={
+            word &&
+            word[5] &&
+            word[5][0] &&
+            ((word[5][0].success && 'success') ||
+              (word[5][0].alert && 'alert') ||
+              (word[5][0].error && 'error'))
+          }
+        >
+          {word && word[5] && word[5][0] && word[5][0].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[5] &&
+            word[5][1] &&
+            ((word[5][1].success && 'success') ||
+              (word[5][1].alert && 'alert') ||
+              (word[5][1].error && 'error'))
+          }
+        >
+          {word && word[5] && word[5][1] && word[5][1].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[5] &&
+            word[5][2] &&
+            ((word[5][2].success && 'success') ||
+              (word[5][2].alert && 'alert') ||
+              (word[5][2].error && 'error'))
+          }
+        >
+          {word && word[5] && word[5][2] && word[5][2].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[5] &&
+            word[5][3] &&
+            ((word[5][3].success && 'success') ||
+              (word[5][3].alert && 'alert') ||
+              (word[5][3].error && 'error'))
+          }
+        >
+          {word && word[5] && word[5][3] && word[5][3].letter}
+        </Letter>
+        <Letter
+          className={
+            word &&
+            word[5] &&
+            word[5][4] &&
+            ((word[5][4].success && 'success') ||
+              (word[5][4].alert && 'alert') ||
+              (word[5][4].error && 'error'))
+          }
+        >
+          {word && word[5] && word[5][4] && word[5][4].letter}
+        </Letter>
       </Row>
     </Container>
   )
