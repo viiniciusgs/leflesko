c969e7367eef6794844895c448f26c791f658b98
Vinicius Gabriel <viiniciusgs03@gmail.com>
2022-03-03 13:19:25 -0300

    :sparkles: add handleButtonClick function in keyboard component

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Row from './Row'
 
+import { useWord } from '../hooks/useWord'
+
 import BackspaceSVG from '../assets/backspace.svg'
 
 import {
@@ -11,44 +13,98 @@ import {
   KeyCustom2,
 } from '../styles/components/Keyboard'
 
-export default function Keyboard() {
+export default function Keyboard({
+  onSubmit,
+  onBackspace,
+}: {
+  onSubmit: () => void
+  onBackspace: () => void
+}) {
+  const { word, handleSetWord, index, finished } = useWord()
+
+  const handleButtonClick = (button: string) => {
+    if (finished) {
+      return
+    }
+
+    if (!word) {
+      handleSetWord([
+        [
+          {
+            letter: button,
+            error: false,
+            alert: false,
+            success: false,
+          },
+        ],
+      ])
+    } else if (!word[index]) {
+      handleSetWord([
+        ...word,
+        [
+          {
+            letter: button,
+            error: false,
+            alert: false,
+            success: false,
+          },
+        ],
+      ])
+    } else {
+      if (word[index].length < 5) {
+        word[index].push({
+          letter: button,
+          error: false,
+          alert: false,
+          success: false,
+        })
+
+        handleSetWord([...word])
+      }
+    }
+  }
+
   return (
     <Container>
       <Row type="custom">
-        <Key>q</Key>
-        <Key>w</Key>
-        <Key>e</Key>
-        <Key>r</Key>
-        <Key>t</Key>
-        <Key>y</Key>
-        <Key>u</Key>
-        <Key>i</Key>
-        <Key>o</Key>
-        <Key>p</Key>
+        <Key onClick={() => handleButtonClick('q')}>q</Key>
+        <Key onClick={() => handleButtonClick('w')}>w</Key>
+        <Key onClick={() => handleButtonClick('e')}>e</Key>
+        <Key onClick={() => handleButtonClick('r')}>r</Key>
+        <Key onClick={() => handleButtonClick('t')}>t</Key>
+        <Key onClick={() => handleButtonClick('y')}>y</Key>
+        <Key onClick={() => handleButtonClick('u')}>u</Key>
+        <Key onClick={() => handleButtonClick('i')}>i</Key>
+        <Key onClick={() => handleButtonClick('o')}>o</Key>
+        <Key onClick={() => handleButtonClick('p')}>p</Key>
       </Row>
       <Row type="custom">
-        <Key>a</Key>
-        <Key>s</Key>
-        <Key>d</Key>
-        <Key>f</Key>
-        <Key>g</Key>
-        <Key>h</Key>
-        <Key>j</Key>
-        <Key>k</Key>
-        <Key>l</Key>
-        <KeyCustom1>
+        <Key onClick={() => handleButtonClick('a')}>a</Key>
+        <Key onClick={() => handleButtonClick('s')}>s</Key>
+        <Key onClick={() => handleButtonClick('d')}>d</Key>
+        <Key onClick={() => handleButtonClick('f')}>f</Key>
+        <Key onClick={() => handleButtonClick('g')}>g</Key>
+        <Key onClick={() => handleButtonClick('h')}>h</Key>
+        <Key onClick={() => handleButtonClick('j')}>j</Key>
+        <Key onClick={() => handleButtonClick('k')}>k</Key>
+        <Key onClick={() => handleButtonClick('l')}>l</Key>
+        <KeyCustom1
+          onClick={() => {
+            onBackspace()
+          }}
+        >
           <BackspaceSVG />
         </KeyCustom1>
       </Row>
       <Row type="custom">
-        <Key>z</Key>
-        <Key>x</Key>
-        <Key>c</Key>
-        <Key>v</Key>
-        <Key>b</Key>
-        <Key>n</Key>
-        <Key>m</Key>
-        <KeyCustom2>enter</KeyCustom2>
+        <Key onClick={() => handleButtonClick('z')}>z</Key>
+        <Key onClick={() => handleButtonClick('x')}>x</Key>
+        <Key onClick={() => handleButtonClick('c')}>c</Key>
+        <Key onClick={() => handleButtonClick('v')}>v</Key>
+        <Key onClick={() => handleButtonClick('b')}>b</Key>
+        <Key onClick={() => handleButtonClick('n')}>n</Key>
+        <Key onClick={() => handleButtonClick('m')}>m</Key>
+        <KeyCustom2 onClick={() => onSubmit()}>enter</KeyCustom2>
       </Row>
     </Container>
   )
